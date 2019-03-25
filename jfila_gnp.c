9375dd33deeffca08505392ccacece9e80721e50
giovanninp <gnp@cesar.school>
2019-03-25 00:32:50 -0300

    fila-tentativa 13

--- jfila_gnp.c
+++ jfila_gnp.c
@@ -1,79 +1,89 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct User{
     int value;
     struct User *next;
 }user;
 
-void show(user *root){
+user *root;
+
+void show(){
     user *tmp;
     tmp = root;
     while (tmp != NULL){
-        if(tmp -> value != 0){
-            printf("%d ",tmp->value);
+        if(tmp->value != 0){
+            printf("%d ",tmp -> value);
         }
         tmp = tmp -> next;
     }
 }
 
-int checkExistence(user *root,int cvalue){
-    int result = 0;
+void insert(int value){
+    user *tmp;
+    tmp = root;
+    while(tmp -> next != NULL){
+        tmp = tmp -> next;
+    }
+    user *newUser = (user *)malloc(sizeof(user));
+    newUser -> value = value;
+    newUser -> next = NULL;
+    tmp -> next = newUser;
+}
+
+void remov(int value){
+    user *tmp,*prev;
+    tmp,prev = root;
+
+    while(tmp != NULL){
+        if(value == tmp -> value){
+            prev -> next = tmp -> next;
+            tmp -> value = 0;
+        }
+        prev = tmp;
+        tmp = tmp -> next;
+    }
+}   
+
+int check(int value){
     user *tmp;
     tmp = root;
     while(tmp != NULL){
-        if (tmp -> value == cvalue){
-            result = 1;
+        if (tmp-> value == value){
+            return value;
         }
         tmp = tmp -> next;
     }
-    return result;
-    
+    return 0;
 }
 
-int main(void) {
-    user *root = (user *)malloc(sizeof(user));
+int main(void){
+    root = (user*)malloc(sizeof(user));
     root -> value = 0;
     root -> next = NULL;
-
-
-
-    int actualValue,n,m;
+    
+    int n,m,actualN,actualM;
 
     scanf("%d",&n);
 
-    for(int i = 0;i < n;i++){
-        scanf("%d",&actualValue);
-        if(checkExistence(root,actualValue) != 1){
-            user *actualUser = (user *)malloc(sizeof(user));
-            actualUser -> value = actualValue;
-            actualUser -> next = NULL;
-
-            user *temp;
-            temp = root;
-            while(temp->next != NULL){
-                temp = temp -> next;
-            }
-            temp -> next = actualUser;
+    for (int i = 0;i<n;i++){
+        scanf("%d",&actualN);
+        if (check(actualN) == 0){
+            insert(actualN);
         }
     }
 
     scanf("%d",&m);
-    for(int j = 0;j < m;j++){
-        scanf("%d",&actualValue);
-        user *tmp,*prev;
-        tmp = root;
-        while(tmp != NULL){
-            if(tmp->value = actualValue){
-                prev -> next = tmp -> next;
-                tmp->value = 0;
-            }
-            prev = tmp;
-            tmp = tmp -> next;
-        }
-        
+
+    for (int j = 0;j < m;j++){
+        scanf("%d",&actualM);
+        remov(actualM);
     }
-    show(root);
+
+    show();
+    
+
+
 
     return 0;
 }
